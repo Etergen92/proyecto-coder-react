15ed6da1f77e236d79b10b615efb966cfd67cafb
Etergen92 <55929647+Etergen92@users.noreply.github.com>
2022-09-02 15:20:46 -0300

    Implementacion de boton contador de producto a añadir a carrito junto a limitador segun cantidad en stocÃk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import NavBar from "./components/NavBar";
 import "./components/app/style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from "./components/ItemListContainer";
+import ItemCount from "./components/ItemCount";
 
 function App() {
   return (
     <div className="App">
       <header><NavBar/></header>
       <ItemListContainer greetings={"Bienvenido a la Disco Tienda"}/>
+      <ItemCount stock={12}/>
+      
     </div>
   );
 }
---
+++ src/components/ItemCount.js
@@ -0,0 +1,24 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { useState } from 'react';
+
+
+
+
+const ItemCount = ({stock}) => {
+  const[cantidad = 1, setContador] = useState()
+  return (
+    <>
+      <div className="estiloContador">
+        <ButtonGroup aria-label="Basic example">
+          <Button onClick={() => cantidad > 0 ? setContador(cantidad - 1) : cantidad}variant="secondary">-</Button>
+          <p className="contadorCantidad d-inline">{cantidad}</p>
+          <Button onClick={() => cantidad < stock ? setContador(cantidad + 1) : cantidad} variant="secondary">+</Button>
+      </ButtonGroup>
+      </div>
+      <Button variant="secondary" className='botonAnadir'>Añadir al Carrito</Button>
+    </>
+  )
+}
+
+export default ItemCount
